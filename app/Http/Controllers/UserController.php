a649b331ad9c4da0913c75d4ad0d96a30286f2dd
Ian Edwards <ianedwards@Ians-Air.home>
2016-01-10 16:02:58 -0500

    Testing controller usage updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 use App\User;
 use App\Http\Controllers\Controller;
 
